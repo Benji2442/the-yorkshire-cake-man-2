6b13b03e94d7c03c0b3d5fc60eef4b1430fb76a4
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-03-26 18:24:42 +0100

    gallery working, work on scrolling to white

--- js/pages.js
+++ js/pages.js
@@ -17,3 +17,19 @@ $(document).on('click','.btn', function(event){
 	buttonPage = buttonPage + ".html";
 	$('#content').load("/pages/" + buttonPage);
 });
+
+$(document).on('click', '.galleryItem a', function(){
+	var selectedCollection = $(this).text().toLowerCase();
+	console.log(selectedCollection);
+	var data = $.getJSON("../json/data.json", function(data) {
+	// console.log(data);
+	var selectedCakes = data.filter(i => i.collection === selectedCollection);
+	console.log(selectedCakes);
+		var cakeHtml = selectedCakes.map(function(i){
+			return "<div class='galleryImage col-lg-4 col-sm-6'><a href='#'><img class='img-thumbnail' src='"+i.thumb+ "' alt=''></a></div>"
+		}).join('');
+		console.log(cakeHtml);
+		$('#cakeHtmlHere').empty().append(cakeHtml);
+
+	});
+});

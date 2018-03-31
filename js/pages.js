7c4c00a3e7ee78470379b660940ac069863ee213
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-03-31 21:09:46 +0100

    before adding lightbox

--- js/pages.js
+++ js/pages.js
@@ -1,7 +1,3 @@
-// $(document).ready(function(){
-// 	$('#content').load('/pages/home.html');
-// });
-
 // below loads page based on nav-link.text
 
 $('.nav-link').click(function(event){
@@ -19,21 +15,4 @@ $(document).on('click','.btn', function(event){
 	var buttonPage = $(this).text().toLowerCase();
 	buttonPage = buttonPage + ".html";
 	$('#content').load("/pages/" + buttonPage);
-});
-
-//below adds photo thumbs in via json data
-
-$(document).on('click', '.galleryItem a', function(){
-	var selectedCollection = $(this).text().toLowerCase();
-	// console.log(selectedCollection);
-	var data = $.getJSON("../json/data.json", function(data) {
-	// console.log(data);
-	var selectedCakes = data.filter(i => i.collection === selectedCollection);
-	// console.log(selectedCakes);
-		var cakeHtml = selectedCakes.map(function(i){
-			return "<div class='galleryImage col-lg-4 col-sm-6'><a data-name='"+i.name+"' href='#'><img class='img-thumbnail' src='"+i.thumb+ "' alt=''></a></div>"
-		}).join('');
-		// console.log(cakeHtml);
-		$('#cakeHtmlHere').empty().append(cakeHtml);
-	});
 });
